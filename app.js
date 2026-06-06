60f84b4af7a2d8486dba2f4d803edfdad5b38a68
ashutoshroli <ashutoshroli@users.noreply.github.com>
2026-06-07 03:08:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbx_8D0senIFIY0bzH8KOBjcK-OZ1TTTj1YPzGfCWkxYdRrkPCVB0VnbX6f-Ng2yxIZm/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwEhsKonoUzRLXW8P25HXlKO3e6UWAy9dE-oJXIBuYcHKJC1nNc96ABnFOQkh-Yur8s/exec";
 
 async function login() {
     const email = document.getElementById('email').value;
     const password = document.getElementById('pass').value;
 
-    const response = await fetch(API_URL, {
-        method: 'POST',
-        mode: 'no-cors', // CORS handling
-        body: JSON.stringify({ action: 'LOGIN', email, password })
-    });
-    
-    alert("Check Console for Response (CORS restricted mode)");
-    console.log("Login Attempted");
+    try {
+        const response = await fetch(API_URL, {
+            method: 'POST',
+            body: JSON.stringify({ action: 'LOGIN', email, password })
+        });
+        const data = await response.json();
+        
+        if (data.status === 'success') {
+            alert("Login Successful: " + data.user.name);
+        } else {
+            alert("Error: " + data.message);
+        }
+    } catch (error) {
+        console.error("Connection Error:", error);
+    }
 }
